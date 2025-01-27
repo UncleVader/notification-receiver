24ac08916a536b891f8c0f7c59ecf76ac79a63e1
Vadym <unclevader@gmail.com>
2025-01-27 08:55:07 +0000

    initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,78 @@
+import {Image, StyleSheet, Platform, Clipboard,} from 'react-native';
+
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import * as Notifications from 'expo-notifications';
+import {useEffect, useRef, useState} from "react"; // Optional
+import {registerForPushNotificationsAsync} from '@/lib/utils'
+
+export default function HomeScreen() {
+  const [expoPushToken, setExpoPushToken] = useState<string | undefined>("")
+  const [notification, setNotification] = useState<Notifications.Notification | null>(null)
+  const notificationListener = useRef<Notifications.Subscription>()
+  const responseListener = useRef<Notifications.Subscription>()
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => setExpoPushToken(token))
+
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      setNotification(notification)
+    })
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log(response)
+    })
+
+    return () => {
+      if (notificationListener.current) Notifications.removeNotificationSubscription(notificationListener.current)
+      if (responseListener.current) Notifications.removeNotificationSubscription(responseListener.current)
+    }
+  }, [])
+
+const copyToClipboard = async () => {
+  Clipboard.setString(expoPushToken||'no token found');
+};
+
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Your push token below</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText
+          type="subtitle"
+          onPress={copyToClipboard}
+        >{expoPushToken}</ThemedText>
+      </ThemedView>
+
+
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
---
+++ lib/utils.ts
@@ -0,0 +1,80 @@
+import * as Notifications from "expo-notifications";
+import {Platform} from "react-native";
+import * as Device from "expo-device";
+import Constants from "expo-constants";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+export async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+
+    if (Constants.easConfig?.projectId) {
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId: Constants.easConfig.projectId, // you can hard code project id if you dont want to use expo Constants
+        })
+      ).data;
+      console.log(token);
+    }
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  return token;
+}
+
+
+export async function sendPushNotification(expoPushToken?: string) {
+  if (!expoPushToken) {
+    console.error("No push token available")
+    return
+  }
+
+  const message = {
+    to: expoPushToken,
+    sound: "default",
+    title: "Test Notification",
+    body: "This is a test notification from your MVP app!",
+    data: { someData: "goes here" },
+  }
+
+  await fetch("https://exp.host/--/api/v2/push/send", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Accept-encoding": "gzip, deflate",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  })
+}
