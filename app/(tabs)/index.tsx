f9fc33d91fcc792ec015fa2ab5d154419000bc9e
Vadym <unclevader@gmail.com>
2025-01-30 11:33:44 +0000

    upwork url supporting added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
-import {Image, StyleSheet, Platform, Clipboard,} from 'react-native';
+import {Image, StyleSheet, Clipboard} from 'react-native';
+import * as Linking from 'expo-linking';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {ThemedText} from '@/components/ThemedText';
+import {ThemedView} from '@/components/ThemedView';
 import * as Notifications from 'expo-notifications';
 import {useEffect, useRef, useState} from "react"; // Optional
 import {registerForPushNotificationsAsync} from '@/lib/utils'
+import {SchedulableTriggerInputTypes} from "expo-notifications";
 
 export default function HomeScreen() {
   const [expoPushToken, setExpoPushToken] = useState<string | undefined>("")
@@ -17,11 +19,15 @@ export default function HomeScreen() {
     registerForPushNotificationsAsync().then((token) => setExpoPushToken(token))
 
     notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      Notifications.cancelAllScheduledNotificationsAsync()
       setNotification(notification)
     })
 
     responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
-      console.log(response)
+      const jobUrl = response?.notification?.request?.content?.data?.url
+      if (jobUrl) {
+        Linking.openURL(`https://www.upwork.com${jobUrl}`)
+      }
     })
 
     return () => {
@@ -30,13 +36,27 @@ export default function HomeScreen() {
     }
   }, [])
 
-const copyToClipboard = async () => {
-  Clipboard.setString(expoPushToken||'no token found');
-};
+  const copyToClipboard = async () => {
+    Clipboard.setString(expoPushToken || 'no token found');
+  };
+
+  const sendLocalTest = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got notification! 🔔",
+        body: 'Here is the notification body',
+        data: { url: 'https://www.upwork.com' },
+      },
+      trigger: {
+        seconds: 2,
+        type: SchedulableTriggerInputTypes.TIME_INTERVAL,
+      },
+    });
+  }
 
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{light: '#A1CEDC', dark: '#1D3D47'}}
       headerImage={
         <Image
           source={require('@/assets/images/partial-react-logo.png')}
@@ -53,6 +73,9 @@ const copyToClipboard = async () => {
         >{expoPushToken}</ThemedText>
       </ThemedView>
 
+      {/*<ThemedView style={styles.titleContainer}>*/}
+      {/*  <Button title={"Send test1"} onPress={sendLocalTest}/>*/}
+      {/*</ThemedView>*/}
 
     </ParallaxScrollView>
   );
